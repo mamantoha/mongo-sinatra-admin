c55708413a883e299c70e377cd3562278dc50a02
Anton Maminov <anton.linux@gmail.com>
2015-07-16 16:06:05 +0300

    initial support of collections

--- app/routes/collection.rb
+++ app/routes/collection.rb
@@ -2,6 +2,14 @@ module MongoAdmin
   class App < Sinatra::Base
 
     get '/db/:database/:collection' do
+      @db_name = params['database']
+      @collection_name = params['collection']
+
+      database = @db.connect(@db_name)
+      collection = database[@collection_name]
+      # Get all documents in a collection
+      @documents = collection.find
+
       slim :collection
     end
 
