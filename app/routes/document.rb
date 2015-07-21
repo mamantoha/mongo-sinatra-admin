556766159bc705f52e90683436b345ac05dbbde1
Anton Maminov <anton.linux@gmail.com>
2015-07-21 12:00:28 +0300

    many improvements for document
    use mongo object id instead of string id

--- app/routes/document.rb
+++ app/routes/document.rb
@@ -1,28 +1,68 @@
 module MongoAdmin
   class App < Sinatra::Base
 
+    get '/db/:database/:collection/new' do
+      @db_name = params['database']
+      @collection_name = params['collection']
+
+      slim :'document/new'
+    end
+
+    # Show Document
     get '/db/:database/:collection/:id' do
       @db_name = params['database']
       @collection_name = params['collection']
-      @document_id = params['id']
+
+      # convert id string to mongodb object id
+      @document_id = BSON::ObjectId.from_string(params[:id])
 
       database = @db.connect(@db_name)
       collection = database[@collection_name]
 
       @document = collection.find({_id: @document_id}).first # BSON::Document
 
-      slim :document
+      if @document
+        slim :'document/edit'
+      else
+        flash[:danger] = 'Document not found.'
+        redirect back
+      end
     end
 
-
-    put '/db/:database/:collection/:id' do
+    # Create new Document
+    post '/db/:database/:collection' do
       db_name         = params['database']
       collection_name = params['collection']
-      document_id     = params['id']
       document        = params['document']
 
       # TODO: document is not valid
       document = JSON.load(document)
+
+      database = @db.connect(db_name)
+      collection = database[collection_name]
+
+      result = collection.insert_one(document)
+      result.n # => returns 1, because 1 document was inserted
+      document_id = result.inserted_id
+
+      flash[:info] = 'New document successfully created.'
+      redirect "/db/#{db_name}/#{collection_name}/#{document_id}"
+    end
+
+    # Update Document
+    put '/db/:database/:collection/:id' do
+      db_name         = params['database']
+      collection_name = params['collection']
+      document        = params['document']
+
+      # convert id string to mongodb object id
+      document_id = BSON::ObjectId.from_string(params[:id])
+
+      # TODO: document is not valid
+      document = JSON.load(document)
+
+      # IMPORTANT! You can not change Object ID
+      document.delete('_id')
 
       database = @db.connect(db_name)
       collection = database[collection_name]
@@ -34,20 +74,31 @@ module MongoAdmin
       redirect "/db/#{db_name}/#{collection_name}/#{document_id}"
     end
 
+    # Destroy Document
     delete '/db/:database/:collection/:id' do
       db_name         = params['database']
       collection_name = params['collection']
-      document_id     = params['id']
       document        = params['document']
 
       database = @db.connect(db_name)
       collection = database[collection_name]
 
-      document = collection.find({_id: document_id}).delete_one
+      # convert id string to mongodb object id
+      document_id = BSON::ObjectId.from_string(params[:id])
 
-      flash[:info] = 'Document successfully removed.'
+      document = collection.find({_id: document_id})
 
-      redirect "/db/#{db_name}/#{collection_name}"
+      if document.first
+        document.delete_one
+        flash[:info] = 'Document successfully removed.'
+
+        redirect "/db/#{db_name}/#{collection_name}"
+      else
+        flash[:danger] = 'Document not found.'
+
+        redirect "/db/#{db_name}/#{collection_name}"
+      end
+
     end
 
   end
