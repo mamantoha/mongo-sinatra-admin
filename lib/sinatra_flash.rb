e32683b99f7b812a04697f15c4c58e0918b006eb
Anton Maminov <anton.linux@gmail.com>
2024-02-26 19:41:49 +0200

    Update sinatra_flash.rb

--- lib/sinatra_flash.rb
+++ lib/sinatra_flash.rb
@@ -10,7 +10,7 @@ module Sinatra
         close = '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
 
         messages = flash(key).collect do |message|
-          "  <div class='alert alert-#{message[0]} alert-dismissible fade show' role='alert'>\n <p>#{message[1]}</p>\n#{close}</div>\n"
+          "  <div class='alert alert-#{message[0]} alert-dismissible fade show' role='alert'>\n <span>#{message[1]}</span>\n#{close}</div>\n"
         end
 
         "<div id='#{id}'>\n#{messages.join}</div>"
